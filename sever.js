81ad9655959606bc397995a7cd930c2cacc01072
Michael Williams <manicdesigner@gmail.com>
2020-03-16 22:53:57 -0400

    add authRotuer to server

--- sever.js
+++ sever.js
@@ -1,15 +1,25 @@
 /** @format */
 
 const express = require('express');
-
 const cors = require('cors');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const authRouter = require('./auth/auth-router');
 
 const router = express();
 router.use(helmet());
 router.use(cors());
 router.use(cookieParser());
 router.use(express.json());
+
+router.use('/api', authRouter);
+
+router.use((req, res) => {
+  res.status(404).json({ message: '404 page fail whale' });
+});
+
+router.use((err, req, res, next) => {
+  res.status(500).json({ message: 'internal server error' });
+});
 
 module.exports = router;
